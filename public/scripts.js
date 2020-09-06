57a7b487044f4ec87d598c5607bf3c91783ce435
vaishak <vaishak.k@zohocorp.com>
2020-09-06 15:29:03 +0530

    UI related changes and data updation

--- public/scripts.js
+++ public/scripts.js
@@ -2,29 +2,36 @@ if ('geolocation' in navigator) {
     console.log("Geo location available");
     navigator.geolocation.getCurrentPosition(async position => {
 
-        console.log(position.coords);
         let lat = position.coords.latitude;
         let long = position.coords.longitude;
 
         document.getElementById("lat").textContent = lat;
         document.getElementById("long").textContent = long;
+        document.getElementById("plocat").classList.add("onresp");
 
         //requesting weather from our server - which inturns gets it from openweathermaps 
         const api_url = `/weather/${lat},${long}`
-            // const api_url = "https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3390a7b37cd01bc9c720fa08459631b2"
+        // const api_url = "https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3390a7b37cd01bc9c720fa08459631b2"
         const response = await fetch(api_url);
         const jsonData = await response.json();
 
         const weather = jsonData.weather;
         const air = jsonData.airquality;
-        const airData = air.results.length > 0 ? air.results[0].measurements[0] : { value: -1 };
+        const airData = air.results.length > 0 ? air.results[0].measurements[0] : undefined;
 
-        document.getElementById("summary").textContent = weather.weather[0].description;
-        document.getElementById("temperature").textContent = weather.main.temp;
-        document.getElementById("airquality").textContent = airData.value > 0 ? airData.value : "NULL";
-        document.getElementById("aq_units").textContent = airData.unit ? airData.unit : "";
-        document.getElementById("last_updated").textContent = airData.lastUpdated ? airData.lastUpdated : "Never";
-        console.log(jsonData);
+        if(weather.weather){
+            document.getElementById("summary").textContent = weather.weather[0].description;
+            document.getElementById("temperature").textContent = weather.main.temp;
+            document.getElementById("pweather").classList.add("onresp");
+        }
+       
+        if(airData){
+            document.getElementById("airquality").textContent = airData.value;
+            document.getElementById("aq_units").textContent = airData.unit ? airData.unit : "";
+            document.getElementById("last_updated").textContent = airData.lastUpdated ? airData.lastUpdated : "Never";
+            document.getElementById("pair").classList.add("onresp");
+        }
+        
 
         //----USING PROMISE -----//
         // fetch('/api', options).then(response => {
@@ -55,9 +62,14 @@ if ('geolocation' in navigator) {
             let response = await fetch('/api', options);
             let jsonData = await response.json();
             console.log(jsonData);
+
+            // Showing the Snackbar 
+            var x = document.getElementById("snackbar");
+            x.className = "show";
+            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
         });
 
     });
 } else {
-    console.log("Geo location not available");
+    alert("Geo location not available");
 }

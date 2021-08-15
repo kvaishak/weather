248774b05dba2e8b3d6d7c2dd50170f45143986c
kvaishak <kvaishak47@gmail.com>
2021-08-15 13:26:34 +0200

    temporarily removed the air quality date due to the api changes

--- index.js
+++ index.js
@@ -1,65 +1,65 @@
 // importing the express framework
-const express = require('express');
-const Datastore = require('nedb');
-const fetch = require('node-fetch');
-require('dotenv').config();
+const express = require("express");
+const Datastore = require("nedb");
+const fetch = require("node-fetch");
+require("dotenv").config();
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Listening at " + port);
+  console.log("Listening at " + port);
 });
 
 //hosting the public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //initialising the database
-const database = new Datastore('database.db');
+const database = new Datastore("database.db");
 database.loadDatabase();
 
 //creating an end point for the client to post the data
-app.use(express.json({ limit: '1mb' })); //for making the request to the server to be parsed as JSON
-app.post('/api', (request, response) => {
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
+app.use(express.json({ limit: "1mb" })); //for making the request to the server to be parsed as JSON
+app.post("/api", (request, response) => {
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
 
-    database.insert(data);
+  database.insert(data);
 
-    response.json(data);
+  response.json(data);
 });
 
 //endpoint for the client to get data
-app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
-        if (err) {
-            response.json({
-                'status': 'Error in fetching data'
-            });
-        }
-        response.json(data);
-    })
+app.get("/api", (request, response) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      response.json({
+        status: "Error in fetching data",
+      });
+    }
+    response.json(data);
+  });
 });
 
 //endpoint for weather request
-app.get('/weather/:latlong', async(request, response) => {
-    const latlong = request.params.latlong.split(',');
+app.get("/weather/:latlong", async (request, response) => {
+  const latlong = request.params.latlong.split(",");
 
-    console.log(latlong);
-    const lat = latlong[0];
-    const long = latlong[1];
-    const apiKey = process.env.API_KEY;
+  console.log(latlong);
+  const lat = latlong[0];
+  const long = latlong[1];
+  const apiKey = process.env.API_KEY;
 
-    const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
-    const weather_response = await fetch(weather_url);
-    const weatherData = await weather_response.json();
+  const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
+  const weather_response = await fetch(weather_url);
+  const weatherData = await weather_response.json();
 
-    const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${long}`;
-    const aq_response = await fetch(aq_url);
-    const aqData = await aq_response.json();
+  // const aq_url = `https://api.openaq.org/v1/latest?coordinates=${lat},${long}`;
+  // const aq_response = await fetch(aq_url);
+  // const aqData = await aq_response.json();
 
-    const data = { weather: weatherData, airquality: aqData };
+  const data = { weather: weatherData }; //const data = { weather: weatherData, airquality: aqData };
 
-    response.json(data);
+  response.json(data);
 });
--- public/scripts.js
+++ public/scripts.js
@@ -1,75 +1,82 @@
-if ('geolocation' in navigator) {
-    console.log("Geo location available");
-    navigator.geolocation.getCurrentPosition(async position => {
+if ("geolocation" in navigator) {
+  console.log("Geo location available");
+  navigator.geolocation.getCurrentPosition(async (position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
 
-        let lat = position.coords.latitude;
-        let long = position.coords.longitude;
+    document.getElementById("lat").textContent = lat;
+    document.getElementById("long").textContent = long;
+    document.getElementById("plocat").classList.add("onresp");
 
-        document.getElementById("lat").textContent = lat;
-        document.getElementById("long").textContent = long;
-        document.getElementById("plocat").classList.add("onresp");
+    //requesting weather from our server - which inturns gets it from openweathermaps
+    const api_url = `/weather/${lat},${long}`;
+    // const api_url = "https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3390a7b37cd01bc9c720fa08459631b2"
+    const response = await fetch(api_url);
+    const jsonData = await response.json();
 
-        //requesting weather from our server - which inturns gets it from openweathermaps 
-        const api_url = `/weather/${lat},${long}`
-        // const api_url = "https://samples.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3390a7b37cd01bc9c720fa08459631b2"
-        const response = await fetch(api_url);
-        const jsonData = await response.json();
+    const weather = jsonData.weather;
+    // const air = jsonData.airquality;
+    // const airData = air.results.length > 0 ? air.results[0].measurements[0] : undefined;
 
-        const weather = jsonData.weather;
-        const air = jsonData.airquality;
-        const airData = air.results.length > 0 ? air.results[0].measurements[0] : undefined;
+    if (weather.weather) {
+      document.getElementById("summary").textContent =
+        weather.weather[0].description;
+      document.getElementById("temperature").textContent = weather.main.temp;
+      document.getElementById("pweather").classList.add("onresp");
+    }
 
-        if(weather.weather){
-            document.getElementById("summary").textContent = weather.weather[0].description;
-            document.getElementById("temperature").textContent = weather.main.temp;
-            document.getElementById("pweather").classList.add("onresp");
-        }
-       
-        if(airData){
-            document.getElementById("airquality").textContent = airData.value;
-            document.getElementById("aq_units").textContent = airData.unit ? airData.unit : "";
-            document.getElementById("last_updated").textContent = airData.lastUpdated ? airData.lastUpdated.substring(0,10) : "Never";
-            document.getElementById("pair").classList.add("onresp");
-        }
-        
+    if (false) {
+      //airData
+      document.getElementById("airquality").textContent = airData.value;
+      document.getElementById("aq_units").textContent = airData.unit
+        ? airData.unit
+        : "";
+      document.getElementById("last_updated").textContent = airData.lastUpdated
+        ? airData.lastUpdated.substring(0, 10)
+        : "Never";
+      document.getElementById("pair").classList.add("onresp");
+    }
 
-        //----USING PROMISE -----//
-        // fetch('/api', options).then(response => {
-        //     // console.log(response);
-        //     return response.json();
-        // }).then(jsonData => {
-        //     console.log(jsonData);
-        // });
+    //----USING PROMISE -----//
+    // fetch('/api', options).then(response => {
+    //     // console.log(response);
+    //     return response.json();
+    // }).then(jsonData => {
+    //     console.log(jsonData);
+    // });
 
-        //----USING ASYNC-AWAIT-------/
-        document.getElementById('sendData').addEventListener('click', async event => {
-            //Data preperation
+    //----USING ASYNC-AWAIT-------/
+    document
+      .getElementById("sendData")
+      .addEventListener("click", async (event) => {
+        //Data preperation
 
-            let data = {
-                lat,
-                long,
-                weather,
-                airData
-            };
-            let options = {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            };
+        let data = {
+          lat,
+          long,
+          weather,
+          // airData
+        };
+        let options = {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(data),
+        };
 
-            let response = await fetch('/api', options);
-            let jsonData = await response.json();
-            console.log(jsonData);
+        let response = await fetch("/api", options);
+        let jsonData = await response.json();
+        console.log(jsonData);
 
-            // Showing the Snackbar 
-            var x = document.getElementById("snackbar");
-            x.className = "show";
-            setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-        });
-
-    });
+        // Showing the Snackbar
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function () {
+          x.className = x.className.replace("show", "");
+        }, 3000);
+      });
+  });
 } else {
-    alert("Geo location not available");
+  alert("Geo location not available");
 }
